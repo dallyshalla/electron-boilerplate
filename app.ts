dd87fe93cae19d1edb17290dbf3ba35cf40648b0
dallyshalla <dallyshalla@gmail.com>
2016-06-22 19:46:57 +0200

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,11 @@
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {Component} from "@angular/core";
+
+@Component({
+	selector: 'app',
+	template: '<h1>hello</h1>'
+})
+
+export class AppComponent {}
+
+bootstrap(AppComponent);
